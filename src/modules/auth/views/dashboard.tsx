efc74016d3ebbeb2d19508f9c1e1768535ba4524
Rizwin A R <arrizwin@skyslit.com>
2023-05-26 18:27:47 +0530

    implemented antd drawer in dashboard page

--- src/modules/auth/views/dashboard.tsx
+++ src/modules/auth/views/dashboard.tsx
@@ -1,12 +1,82 @@
 import React from "react";
 import { createComponent, Frontend } from "@skyslit/ark-frontend";
 import '../styles/dashboard.scss';
-import { Button, Col, Row } from "antd";
+import { Button, Col, Row, Drawer, Input } from "antd";
 import Fade from "react-reveal/Fade";
 import moment from 'moment';
-import { DashboardAddIcon } from "../../auth/icons/global-icons";
+import { DashboardAddIcon, LeftArrowIcon, PieDiagramIcon, SearchIcon } from "../../auth/icons/global-icons";
+
+const Widget = createComponent((props) => {
+
+    return (
+        <>
+            <div className="single-dashboard-content-box">
+                <PieDiagramIcon className="pie-chart-icon" />
+                <span className="heading-text">Monthly Comparison Chart</span>
+                <span className="description-text">Shows the comparison between monthly budget and actual expense. Shows the comparison between monthly budget and actual expense.</span>
+                <div className="add-dashboard-btn-wrapper">
+                    <Button type="text" className="add-dashboard-btn">Add to dashboard</Button>
+                </div>
+            </div>
+
+            <div className="single-dashboard-content-box">
+                <PieDiagramIcon className="pie-chart-icon" />
+                <span className="heading-text">Monthly Comparison Chart</span>
+                <span className="description-text">Shows the comparison between monthly budget and actual expense. Shows the comparison between monthly budget and actual expense.</span>
+                <div className="add-dashboard-btn-wrapper">
+                    <Button type="text" className="add-dashboard-btn">Add to dashboard</Button>
+                </div>
+            </div>
+
+            <div className="single-dashboard-content-box">
+                <PieDiagramIcon className="pie-chart-icon" />
+                <span className="heading-text">Monthly Comparison Chart</span>
+                <span className="description-text">Shows the comparison between monthly budget and actual expense. Shows the comparison between monthly budget and actual expense.</span>
+                <div className="add-dashboard-btn-wrapper">
+                    <Button type="text" className="add-dashboard-btn">Add to dashboard</Button>
+                </div>
+            </div>
+
+            <div className="single-dashboard-content-box">
+                <PieDiagramIcon className="pie-chart-icon" />
+                <span className="heading-text">Monthly Comparison Chart</span>
+                <span className="description-text">Shows the comparison between monthly budget and actual expense. Shows the comparison between monthly budget and actual expense.</span>
+                <div className="add-dashboard-btn-wrapper">
+                    <Button type="text" className="add-dashboard-btn">Add to dashboard</Button>
+                </div>
+            </div>
+        </>
+    );
+});
+
+const DrawerContent = createComponent((props) => {
+
+    return (
+        <div style={{ display: "flex" }}>
+            <div className="sider-whole-wrapper" >
+                <div className="sider-content-wrapper">
+                    <Button className="selected-sider-btn" type="text">Finance (5)</Button>
+                    <Button className="sider-btn" type="text">Finance (1) </Button>
+                </div>
+            </div>
+            <div className="drawer-main-content-wrapper">
+                <Widget {...props} />
+            </div>
+        </div>
+    );
+});
 
 export default createComponent((props) => {
+
+    const [visible, setVisible] = React.useState(false);
+
+    const showDrawer = () => {
+        setVisible(true);
+    };
+
+    const hideDrawer = () => {
+        setVisible(false);
+    };
 
     return (
         <Fade duration={700}>
@@ -19,7 +89,7 @@ export default createComponent((props) => {
                                 <span className="dashboard-text">Dashboard</span>
                             </div>
                             <div className="dashboard-add-icon-wrapper">
-                                <Button className="dashboard-add-icon-btn" type="text">
+                                <Button onClick={showDrawer} className="dashboard-add-icon-btn" type="text">
                                     <DashboardAddIcon style={{ fontSize: 22 }} />
                                 </Button>
                             </div>
@@ -27,6 +97,32 @@ export default createComponent((props) => {
                     </Col>
                 </Row>
             </div>
+            <Drawer className="drawer-wrapper"
+                placement="right"
+                closable={false}
+                onClose={hideDrawer}
+                visible={visible}
+                title={[
+                    <div className="header-wrapper">
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <Button onClick={hideDrawer} className="left-arrow-back-btn" type="text">
+                                <LeftArrowIcon style={{ color: "#2B2B2B", fontSize: 13 }} />
+                            </Button>
+                            <span className="widget-gallery-text">Widget Gallery</span>
+                        </div>
+                        <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
+                            <SearchIcon style={{ position: "absolute", zIndex: 100, left: 13, top: 12, fontSize: 13 }} />
+                            <Input
+                                className='custom-select-solution-input'
+                                style={{ width: 283 }}
+                                placeholder="Search for widgets"
+                            />
+                        </div>
+                    </div>
+                ]}
+            >
+                <DrawerContent  {...props} />
+            </Drawer>
         </Fade>
     );
 });
