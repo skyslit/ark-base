56400b004f30fd8117d399bbd2e01f207c2e70b9
Rizwin A R <arrizwin@skyslit.com>
2023-05-30 18:33:57 +0530

    added helmet to dashboard page

--- src/modules/auth/views/dashboard.tsx
+++ src/modules/auth/views/dashboard.tsx
@@ -5,6 +5,7 @@ import { Button, Col, Row, Drawer, Input } from "antd";
 import Fade from "react-reveal/Fade";
 import moment from 'moment';
 import { DashboardAddIcon, LeftArrowIcon, PieDiagramIcon, SearchIcon } from "../../auth/icons/global-icons";
+import { Helmet } from "react-helmet-async";
 
 const Widget = createComponent((props) => {
 
@@ -79,50 +80,56 @@ export default createComponent((props) => {
     };
 
     return (
-        <Fade duration={700}>
-            <div className="dashboard-wrapper">
-                <Row style={{ background: "#F8F8F8" }} justify="center">
-                    <Col className="dashboard-content-wrapper" span={22}>
-                        <div className="dashboard-text-icon-wrapper" >
-                            <div className="dashboard-date-text-wrapper">
-                                <span className="date-text">{moment().format('dddd, DD MMMM YYYY')}</span>
-                                <span className="dashboard-text">Dashboard</span>
+        <>
+            <Helmet>
+                <title> Dashboard </title>
+            </Helmet>
+            <Fade duration={700}>
+                <div className="dashboard-wrapper">
+                    <Row style={{ background: "#F8F8F8" }} justify="center">
+                        <Col className="dashboard-content-wrapper" span={22}>
+                            <div className="dashboard-text-icon-wrapper" >
+                                <div className="dashboard-date-text-wrapper">
+                                    <span className="date-text">{moment().format('dddd, DD MMMM YYYY')}</span>
+                                    <span className="dashboard-text">Dashboard</span>
+                                </div>
+                                <div className="dashboard-add-icon-wrapper">
+                                    <Button onClick={showDrawer} className="dashboard-add-icon-btn" type="text">
+                                        <DashboardAddIcon style={{ fontSize: 22 }} />
+                                    </Button>
+                                </div>
                             </div>
-                            <div className="dashboard-add-icon-wrapper">
-                                <Button onClick={showDrawer} className="dashboard-add-icon-btn" type="text">
-                                    <DashboardAddIcon style={{ fontSize: 22 }} />
+                        </Col>
+                    </Row>
+                </div>
+                <Drawer className="drawer-wrapper"
+                    placement="right"
+                    closable={false}
+                    onClose={hideDrawer}
+                    visible={visible}
+                    title={[
+                        <div className="header-wrapper">
+                            <div style={{ display: "flex", alignItems: "center" }}>
+                                <Button onClick={hideDrawer} className="left-arrow-back-btn" type="text">
+                                    <LeftArrowIcon style={{ color: "#2B2B2B", fontSize: 13 }} />
                                 </Button>
+                                <span className="widget-gallery-text">Widget Gallery</span>
+                            </div>
+                            <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
+                                <SearchIcon style={{ position: "absolute", zIndex: 100, left: 13, top: 12, fontSize: 13 }} />
+                                <Input
+                                    className='custom-select-solution-input'
+                                    style={{ width: 283 }}
+                                    placeholder="Search for widgets"
+                                />
                             </div>
                         </div>
-                    </Col>
-                </Row>
-            </div>
-            <Drawer className="drawer-wrapper"
-                placement="right"
-                closable={false}
-                onClose={hideDrawer}
-                visible={visible}
-                title={[
-                    <div className="header-wrapper">
-                        <div style={{ display: "flex", alignItems: "center" }}>
-                            <Button onClick={hideDrawer} className="left-arrow-back-btn" type="text">
-                                <LeftArrowIcon style={{ color: "#2B2B2B", fontSize: 13 }} />
-                            </Button>
-                            <span className="widget-gallery-text">Widget Gallery</span>
-                        </div>
-                        <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
-                            <SearchIcon style={{ position: "absolute", zIndex: 100, left: 13, top: 12, fontSize: 13 }} />
-                            <Input
-                                className='custom-select-solution-input'
-                                style={{ width: 283 }}
-                                placeholder="Search for widgets"
-                            />
-                        </div>
-                    </div>
-                ]}
-            >
-                <DrawerContent  {...props} />
-            </Drawer>
-        </Fade>
+                    ]}
+                >
+                    <DrawerContent  {...props} />
+                </Drawer>
+            </Fade>
+        </>
+
     );
 });
