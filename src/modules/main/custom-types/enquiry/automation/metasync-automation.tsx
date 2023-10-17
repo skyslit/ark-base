6a72e99984d422dff419e6cc3465fe4477d2b7b4
Asif Muhammed <asif.muhammed@skyslit.com>
2023-10-17 12:20:50 +0530

    refactor: added new custom type enquiry

--- src/modules/main/custom-types/enquiry/automation/metasync-automation.tsx
+++ src/modules/main/custom-types/enquiry/automation/metasync-automation.tsx
@@ -1,7 +1,6 @@
 import { Automators } from "@skyslit/ark-backend/build/dynamics-v2";
 
 export const EnquiryMetaSyncAutomator =  Automators.createMetaSyncAutomator((api) => {
-    console.log("hh",api.file)
       api.updateMeta({
         name: api.file.name,
         email: api.file.email,
